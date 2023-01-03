8953395e09c195fc5deeecdc9a47db8dbfae2d3c
Luciano Mazzuca <mazzucaluciano@hotmail.com>
2023-01-03 01:52:41 -0300

    Modifica estilos

--- frontend/src/_metronic/layout/components/Layout.js
+++ frontend/src/_metronic/layout/components/Layout.js
@@ -10,7 +10,7 @@ import {Footer} from "./footer/Footer";
 import {LayoutInit} from "./LayoutInit";
 import {SubHeader} from "./subheader/SubHeader";
 import {QuickPanel} from "./extras/offcanvas/QuickPanel";
-// import {QuickUser} from "./extras/offcanvas/QuickUser";
+import {QuickUser} from "./extras/offcanvas/QuickUser";
 import {ScrollTop} from "./extras/ScrollTop";
 import {StickyToolbar} from "./extras/StickyToolbar";
 
@@ -49,7 +49,7 @@ export function Layout({ children }) {
                             id="kt_content"
                             className={`content ${layoutProps.contentCssClasses} d-flex flex-column flex-column-fluid`}
                         >
-                            {/* {layoutProps.subheaderDisplay && <SubHeader/>} */}
+                            {layoutProps.subheaderDisplay && <SubHeader/>}
                             {!layoutProps.contentExtended && (
                                 <div className="d-flex flex-column-fluid">
                                     <div className={layoutProps.contentContainerClasses}>
@@ -69,10 +69,10 @@ export function Layout({ children }) {
                 {/*end::Page*/}
                 
             </div>
-            {/* <QuickUser/> */}
+            <QuickUser/>
             <QuickPanel/>
             <ScrollTop/>
-            <StickyToolbar/>
+            {/* <StickyToolbar/> */}
             {/*end::Main*/}
             <LayoutInit />
         </>
